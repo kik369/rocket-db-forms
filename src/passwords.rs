6d139ccd406fcb2425b37a3883b409cd693b5bea
Kris <kris@refakor.dev>
2023-11-25 21:16:50 +0000

    delete lines from passwords

--- src/passwords.rs
+++ src/passwords.rs
@@ -1,15 +1,3 @@
-// extern crate bcrypt;
-
-// use bcrypt::{hash, verify, DEFAULT_COST};
-
-// pub fn hash_password(password: &str) -> String {
-//     hash(password, DEFAULT_COST).unwrap()
-// }
-
-// pub fn verify_password(password: &str, hashed_password: &str) -> bool {
-//     verify(password, hashed_password).unwrap()
-// }
-
 use bcrypt::{hash, verify, DEFAULT_COST};
 use std::result;
 
